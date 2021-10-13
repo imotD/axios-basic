50f40a0648cf1436854decb7caaa720ed4130c5e
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-10-13 09:18:54 +0700

    => episode global axios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # axios-basic
+
+Tutorial by [Traversy Media](https://www.youtube.com/watch?v=6LyagkoRWYA)
---
+++ main.js
@@ -0,0 +1,145 @@
+// * axios global
+axios.defaults.headers.common["X-Auth-Token"] =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+function getTodos() {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    .then((res) => showOutput(res))
+    .catch((error) => console.log(error));
+}
+
+function addTodos() {
+  axios
+    .post("https://jsonplaceholder.typicode.com/todos", {
+      title: "New Try Axios",
+      completed: false,
+    })
+    .then((res) => showOutput(res))
+    .catch((error) => console.log(error));
+}
+
+function updateTodos() {
+  axios
+    .patch("https://jsonplaceholder.typicode.com/todos/1", {
+      title: "Update Try Axios",
+      completed: true,
+    })
+    .then((res) => showOutput(res))
+    .catch((error) => console.log(error));
+}
+function removeTodos() {
+  axios
+    .delete("https://jsonplaceholder.typicode.com/todos/1", {
+      title: "New Try Axios",
+      completed: false,
+    })
+    .then((res) => showOutput(res))
+    .catch((error) => console.log(error));
+}
+function getData() {
+  axios
+    .all([
+      axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5"),
+      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5"),
+    ])
+    .then(axios.spread((todos, posts) => showOutput(todos)))
+    .catch((error) => console.log(error));
+}
+
+function customHeaders() {
+  const config = {
+    headers: {
+      "content-type": "application/json",
+      Authorization: "ini adalah token",
+    },
+  };
+  axios
+    .post(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        title: "New Try Axios",
+        completed: false,
+      },
+      config
+    )
+    .then((res) => showOutput(res))
+    .catch((error) => console.log(error));
+}
+
+function transformResponse() {
+  const options = {
+    method: "post",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    data: {
+      title: "Halo ini di ubah",
+    },
+    transformResponse: axios.defaults.transformResponse.concat((data) => {
+      data.title = data.title.toUpperCase();
+      return data;
+    }),
+  };
+
+  axios(options).then((res) => showOutput(res));
+}
+
+axios.interceptors.request.use(
+  (config) => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Show output in browser
+function showOutput(res) {
+  document.getElementById("res").innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+    </div>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
+}
+
+// Event listeners
+document.getElementById("get").addEventListener("click", getTodos);
+document.getElementById("post").addEventListener("click", addTodos);
+document.getElementById("update").addEventListener("click", updateTodos);
+document.getElementById("delete").addEventListener("click", removeTodos);
+document.getElementById("sim").addEventListener("click", getData);
+document.getElementById("headers").addEventListener("click", customHeaders);
+document
+  .getElementById("transform")
+  .addEventListener("click", transformResponse);
+document.getElementById("error").addEventListener("click", errorHandling);
+document.getElementById("cancel").addEventListener("click", cancelToken);
